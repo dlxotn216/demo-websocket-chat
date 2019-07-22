44f5980b424b1b6264879dc6021b04f849137829
Lee Tae Su <taesu@crscube.co.kr>
2019-07-22 15:37:15 +0900

    ChatMessage - file repository
    File 저장소 절대경로로 임시 지정

--- src/main/java/demo/app/file/service/FileService.java
+++ src/main/java/demo/app/file/service/FileService.java
@@ -26,11 +26,11 @@ import static org.apache.commons.io.FilenameUtils.getExtension;
 @RequiredArgsConstructor
 public class FileService {
     private static final String DIRECTORY_PATH = "uploads";
-    private final ServletContext servletContext;
+//    private final ServletContext servletContext;
 
     public FileUploadResponse upload(MultipartFile multipartFile) {
         String fileId = UUID.randomUUID().toString() + "." + getExtension(multipartFile.getOriginalFilename());
-        Path directoryPath = Paths.get(this.servletContext.getRealPath("/")).resolve(DIRECTORY_PATH);
+        Path directoryPath = getRootPath().resolve(DIRECTORY_PATH);
         if (!directoryPath.toFile().exists() && !directoryPath.toFile().mkdirs()) {
             throw new IllegalStateException("디렉토리를 생성할 수 없습니다.");
         }
@@ -46,11 +46,18 @@ public class FileService {
     }
 
     public Resource loadFileAsResource(String fileId) {
-        Path filePath = Paths.get(this.servletContext.getRealPath("/")).resolve(DIRECTORY_PATH).resolve(fileId);
+        Path filePath = getRootPath().resolve(DIRECTORY_PATH).resolve(fileId);
         try {
             return new UrlResource(filePath.toUri());
         } catch (MalformedURLException e) {
             throw new IllegalArgumentException("존재하지 않는 경로 [" + filePath.toString() + "]");
         }
+    }
+
+
+    //두 대의 서버를 띄울 경우 공유 파일 서버가 없어서 임시로..
+    private Path getRootPath() {
+        //        return Paths.get(this.servletContext.getRealPath("/"));
+        return Paths.get("/Users/itaesu/Desktop/Work/deploy");
     }
 }
